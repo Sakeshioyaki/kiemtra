85cb4bed88776bf9811c5e84dbbcbc94c4abcd20
Sakeshioyaki <nguyetanh150299@gmail.com>
2021-09-08 11:50:21 +0700

    update

--- test/src/App.js
+++ test/src/App.js
@@ -1,15 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import NameForm from './form';
 
-function App() {
-  const arr = ["Huyen", "Hoa", "Hung", "Long"];
-  return (
-    <div className="App">
-      <div>学生一覧</div>
-      <NameForm/>
-    </div>
-  );
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '', arr: []};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState(({
+      arr: this.state.arr.concat(this.state.value)
+    }))
+
+    event.preventDefault();
+  }
+
+  render() {
+    let arri = this.state.arr;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        <div>{JSON.stringify(arri)}</div>
+      </form>
+    );
+  }
 }
 
 export default App;
